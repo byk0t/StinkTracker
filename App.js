8d1a1956554917b060798dce34293875958fb3f1
Pavel Katovich <pavel.katovich@itechart-group.com>
2019-08-14 14:02:23 +0300

    geolocation

--- App.js
+++ App.js
@@ -15,35 +15,137 @@ import {
   Text,
   StatusBar,
   ImageBackground,
-  Button
+  Button,
+  Alert,
+  PermissionsAndroid
 } from 'react-native';
+import Slider from '@react-native-community/slider';
+import Geolocation from 'react-native-geolocation-service';
 
-const App = () => {
-  return (           
-    <ImageBackground 
-      source={require('./app/background.jpeg')}      
-      style={styles.background}>
-      <View style={styles.container}>
-        <Button              
-          title="Отправить"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-    </ImageBackground>      
-  );
+export default class App extends React.Component {
+  
+  state = {
+    value: 0,
+    position: null,
+  }
+
+
+  componentDidMount() {    
+  }
+
+  render() {
+    return (           
+      <ImageBackground 
+        source={require('./app/background.jpeg')}      
+        style={styles.background}>
+        <View style={styles.container}>
+          <Text style={styles.value}>{this.state.value}</Text>
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={10}
+            step={1}
+            minimumTrackTintColor="red"
+            maximumTrackTintColor="#000000"
+            onValueChange={value => this.setState({value: value})}
+          />
+          <View style={styles.buttonWrapper}>
+            <Button
+              style={styles.button}
+              title="Отправить"            
+              accessibilityLabel="Learn more about this purple button"
+              onPress={ () => this._sendData() }
+            />
+          </View>
+        </View>
+      </ImageBackground>      
+    );
+  }
+
+  async _sendData() {    
+    let position = await this._getPosition();
+    console.warn(position)
+  }
+
+  async _getPosition() {    
+    const hasLocationPermission = await this._requestPositionPermission();
+    if(hasLocationPermission) {
+      Geolocation.getCurrentPosition(
+          (position) => {
+              // save position to the store
+              this.setState({position: position})
+              console.warn(position);
+          },
+          (error) => {
+              // See error code charts below.
+              console.log(error.code, error.message);
+          },
+          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+      );
+    }
+  }
+
+  async _requestPositionPermission() {
+    let result = false;
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Разрешение геолокации',
+          message: 'Для отправки данных, нам нужно знать ваши координаты',
+          buttonNeutral: 'Позже',
+          buttonNegative: 'Завершить',
+          buttonPositive: 'Хорошо',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.warn('You can use the location');
+        result = true;
+      } else {
+        console.warn('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+    return result;
+  }
+
 };
 
 const styles = StyleSheet.create({
   background: {
     width: '100%', 
-    height: '100%',     
+    height: '100%',    
   },
   container: {
     flex:1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  value: {
+    fontWeight: 'bold',
+    fontSize:50,
+    color: 'blue',
+    backgroundColor: 'white',
+    width:100,
+    textAlign:'center',
+    borderRadius: 25,
+    opacity: 0.8
+  },
+  button: {
+    color: '#841584',
+    borderRadius: 25,
+    marginTop:10
+  },
+  buttonWrapper: {
+    marginTop:10,    
+  },
+  slider: {
+    width: 200, 
+    height: 40,
+    backgroundColor: 'white',
+    opacity: 0.8,
+    marginTop:10
   }
 });
 
-export default App;
